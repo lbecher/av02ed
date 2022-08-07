7432be12f5aeeb61a778a32be8657e86850e3aa3
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-08-07 12:37:08 -0300

    Update Fila.java
    Signed-off-by: Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>

--- atividade2/Fila.java
+++ atividade2/Fila.java
@@ -1,3 +1,32 @@
 public class Fila extends Lista {
-  
+	
+	public Fila() {
+		super();
+	}
+	
+	public int get_topo() {
+		return this.get_tamanho();
+	}
+	
+	@Override
+	public void adiciona_no_inicio(Dado dado) {
+		System.out.printf("Método não implementado para Fila!\n");
+	}
+	
+	@Override
+	public void adiciona_na_posicao(int i, Dado dado) {
+		System.out.printf("Método não implementado para Fila!\n");		
+	}
+	
+	@Override
+	public Dado remove_do_fim() {
+		System.out.printf("Método não implementado para Fila!\n");
+		return null;
+	}
+	
+	@Override
+	public Dado remove_da_posicao(int i) {
+		System.out.printf("Método não implementado para Fila!\n");
+		return null;
+	}
 }
