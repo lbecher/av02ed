db18ac088a396fc151b60344e501f0f620419cef
AlessandraDosSantos <66135957+AlessandraDosSantos@users.noreply.github.com>
2022-08-07 20:52:17 -0300

    Create atividade1.h
    Signed-off-by: AlessandraDosSantos <66135957+AlessandraDosSantos@users.noreply.github.com>

---
+++ atividade1/atividade1.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int tipo_vertice;
+
+typedef struct vertice no;
+struct vertice {
+  tipo_vertice nome;
+  no *proximo;
+  float peso;
+};
+
+typedef struct grafo *Grafo;
+struct grafo {
+  no **conexao;
+  int n_vertices;
+  int n_aresta;
+};
+
+Grafo cria(int max) 
+void insere_conexao(Grafo gr, tipo_vertice vertice1, tipo_vertice vertice2, float peso) 
+void imprime(Grafo gr) 
+
