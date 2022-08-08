b7d970dbec34889e1e07296a19c05081c2d45cf5
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-08-07 23:43:04 -0300

    Update Nodo.java
    Signed-off-by: Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>

--- atividade2/Nodo.java
+++ atividade2/Nodo.java
@@ -1,21 +1,24 @@
 public class Nodo {
-	private Dado dado;
-	private Nodo proximo;
+  // atributos
+  private Dado dado;
+  private Nodo proximo;
 
-	public Nodo(Dado dado, Nodo proximo) {
-		this.dado = dado;
-		this.proximo = proximo;
-	}
+  // construtor
+  public Nodo(Dado dado, Nodo proximo) {
+    this.dado = dado;
+    this.proximo = proximo;
+  }
 
-	public void set_proximo(Nodo proximo) {
-		this.proximo = proximo;
-	}
+  // métodos
+  public void set_proximo(Nodo proximo) {
+    this.proximo = proximo;
+  }
 
-	public Dado get_dado() {
-		return this.dado;
-	}
+  public Dado get_dado() {
+    return this.dado;
+  }
 
-	public Nodo get_proximo() {
-		return this.proximo;
-	}
+  public Nodo get_proximo() {
+    return this.proximo;
+  }
 }
