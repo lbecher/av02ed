12462dca7e3430bac8978872d2518feed445d482
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-08-07 23:57:19 -0300

    Update Lista.java
    Signed-off-by: Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>

--- atividade2/Lista.java
+++ atividade2/Lista.java
@@ -1,139 +1,153 @@
 public class Lista {
-	
-	private Nodo cabeca;
-	private int tamanho;
-	
-	public Lista() {
-		this.cabeca = null;
-		this.tamanho = 0;
-	}
+  // atributos
+  private Nodo cabeca;
+  private int tamanho;
+  
+  // construtor
+  public Lista() {
+    this.cabeca = null;
+    this.tamanho = 0;
+  }
 
-	public void adiciona_no_inicio(Dado dado) {
-		Nodo novo = new Nodo(dado, null);
+  // métodos
+  public void adiciona_no_inicio(Dado dado) {
+    Nodo novo = new Nodo(dado, null);
 
-		if (this.tamanho == 0) {
-			this.cabeca = novo;
-			this.tamanho = 1;
-		} else {
-			novo.set_proximo(this.cabeca);
-			this.cabeca = novo;
-			this.tamanho++;
-		}
-	}
+    if (this.tamanho == 0) {
+      this.cabeca = novo;
+      this.tamanho = 1;
+    } else {
+      novo.set_proximo(this.cabeca);
+      this.cabeca = novo;
+      this.tamanho++;
+    }
+  }
 
-	public void adiciona_no_fim(Dado dado) {
-		Nodo novo = new Nodo(dado, null);
+  public void adiciona_no_fim(Dado dado) {
+    Nodo novo = new Nodo(dado, null);
 
-		if (this.tamanho == 0) {
-			this.cabeca = novo;
-			this.tamanho = 1;
-		} else {
-			Nodo nodo = this.cabeca;
-			Nodo anterior = null;
-			while (nodo != null) {
-				anterior = nodo;
-				nodo = nodo.get_proximo();
-			}
-			anterior.set_proximo(novo);
-			this.tamanho++;
-		}
-	}
+    if (this.tamanho == 0) {
+      this.cabeca = novo;
+      this.tamanho = 1;
+    } else {
+      Nodo nodo = this.cabeca;
+      Nodo anterior = null;
+      
+      // encontra o fim da lista
+      while (nodo != null) {
+        anterior = nodo;
+        nodo = nodo.get_proximo();
+      }
+      
+      anterior.set_proximo(novo);
+      this.tamanho++;
+    }
+  }
 
-	public void adiciona_na_posicao(int i, Dado dado) {
-		Nodo novo = new Nodo(dado, null);
-		
-		if (i >= tamanho || i < 0) {
-			System.out.printf("Valor da posição fora do comprimento atual da estrutura de dados!\n");
-		} else if (this.tamanho == 0) {
-			this.cabeca = novo;
-			this.tamanho = 1;
-		} else if (i == 0) {
-			this.adiciona_no_inicio(dado);
-		} else {
-			Nodo nodo = this.cabeca;
-			Nodo anterior = null;
-			for (int j = 0; j < i; j++) {
-				anterior = nodo;
-				nodo = nodo.get_proximo();
-			}
-			novo.set_proximo(nodo);
-			anterior.set_proximo(novo);
-			this.tamanho++;
-		}
-	}
+  public void adiciona_na_posicao(int i, Dado dado) {
+    Nodo novo = new Nodo(dado, null);
+    
+    if (i >= tamanho || i < 0) {
+      System.out.printf("Valor da posição fora do comprimento atual da estrutura de dados!\n");
+    } else if (this.tamanho == 0) {
+      this.cabeca = novo;
+      this.tamanho = 1;
+    } else if (i == 0) {
+      this.adiciona_no_inicio(dado);
+    } else {
+      Nodo nodo = this.cabeca;
+      Nodo anterior = null;
+      
+      // encontra nodo da posição i
+      for (int j = 0; j < i; j++) {
+        anterior = nodo;
+        nodo = nodo.get_proximo();
+      }
+      
+      novo.set_proximo(nodo);
+      anterior.set_proximo(novo);
+      this.tamanho++;
+    }
+  }
 
-	public Dado remove_do_inicio() {
-		if (this.tamanho == 0) {
-			System.out.printf("Estrutura de dados vazia!\n");
-			return null;
-		} else {
-			Dado dado = this.cabeca.get_dado();
-			this.cabeca = this.cabeca.get_proximo();
-			this.tamanho--;
-			return dado;
-		}
-	}
+  public Dado remove_do_inicio() {
+    if (this.tamanho == 0) {
+      System.out.printf("Estrutura de dados vazia!\n");
+      return null;
+    } else {
+      Dado dado = this.cabeca.get_dado();
+      this.cabeca = this.cabeca.get_proximo();
+      this.tamanho--;
+      return dado;
+    }
+  }
 
-	public Dado remove_do_fim() {
-		if (this.tamanho < 2) {
-			return this.remove_do_inicio();
-		} else {
-			Nodo nodo = this.cabeca.get_proximo();
-			Nodo anterior = this.cabeca;
-			Nodo anterior_do_anterior = null;
-			while (nodo != null) {
-				anterior_do_anterior = anterior;
-				anterior = nodo;
-				nodo = nodo.get_proximo();
-			}
-			Dado dado = anterior.get_dado();
-			anterior_do_anterior.set_proximo(null);
-			this.tamanho--;
-			return dado;
-		}
-	}
+  public Dado remove_do_fim() {
+    if (this.tamanho < 2) {
+      return this.remove_do_inicio();
+    } else {
+      Nodo nodo = this.cabeca.get_proximo();
+      Nodo anterior = this.cabeca;
+      Nodo anterior_do_anterior = null;
+      
+      // encontra o fim da lista
+      while (nodo != null) {
+        anterior_do_anterior = anterior;
+        anterior = nodo;
+        nodo = nodo.get_proximo();
+      }
+      
+      Dado dado = anterior.get_dado();
+      anterior_do_anterior.set_proximo(null);
+      this.tamanho--;
+      return dado;
+    }
+  }
 
-	public Dado remove_da_posicao(int i) {
-		if (i >= tamanho || i < 0) {
-			System.out.printf("Valor da posição fora do comprimento atual da estrutura de dados!\n");
-			return null;
-		} else if (this.tamanho < 2) {
-			return this.remove_do_inicio();
-		} else if (this.tamanho == i - 1) {
-			return this.remove_do_fim();
-		} else {
-			Nodo nodo = this.cabeca.get_proximo();
-			Nodo anterior = this.cabeca;
-			Nodo anterior_do_anterior = null;
-			for (int j = 0; j < i; j++) {
-				anterior_do_anterior = anterior;
-				anterior = nodo;
-				nodo = nodo.get_proximo();
-			}
-			Dado dado = anterior.get_dado();
-			anterior_do_anterior.set_proximo(nodo);
-			this.tamanho--;
-			return dado;
-		}
-	}
-	
-	public int get_tamanho() {
-		return this.tamanho;
-	}
+  public Dado remove_da_posicao(int i) {
+    if (i >= tamanho || i < 0) {
+      System.out.printf("Valor da posição fora do comprimento atual da estrutura de dados!\n");
+      return null;
+    } else if (this.tamanho < 2) {
+      return this.remove_do_inicio();
+    } else if (this.tamanho == i - 1) {
+      return this.remove_do_fim();
+    } else {
+      Nodo nodo = this.cabeca.get_proximo();
+      Nodo anterior = this.cabeca;
+      Nodo anterior_do_anterior = null;
+      
+      // encontra nodo da posição i
+      for (int j = 0; j < i; j++) {
+        anterior_do_anterior = anterior;
+        anterior = nodo;
+        nodo = nodo.get_proximo();
+      }
+      
+      Dado dado = anterior.get_dado();
+      anterior_do_anterior.set_proximo(nodo);
+      this.tamanho--;
+      return dado;
+    }
+  }
+  
+  public int get_tamanho() {
+    return this.tamanho;
+  }
 
-	public void printa() {
-		System.out.printf("\nEstrutura de dados: ");
-		if (this.cabeca == null) {
-			System.out.printf("vazia!\n\n");
-		} else {
-			Nodo nodo = this.cabeca;
-			Dado dado = null;
-			while (nodo != null) {
-				dado = nodo.get_dado();
-				System.out.printf("%d ", dado.get_dado());
-				nodo = nodo.get_proximo();
-			}
-			System.out.printf("\n\n");
-		}
-	}
+  public void printa() {
+    System.out.printf("\nEstrutura de dados: ");
+    if (this.cabeca == null) {
+      System.out.printf("vazia!\n\n");
+    } else {
+      Nodo nodo = this.cabeca;
+      Dado dado = null;
+      while (nodo != null) {
+        dado = nodo.get_dado();
+        System.out.printf("%d ", dado.get_dado());
+        nodo = nodo.get_proximo();
+      }
+      System.out.printf("\n\n");
+    }
+  }
 }
