6d282d447d986468ccaeff2c2767578ccb6c5295
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-08-08 00:31:21 -0300

    Create main.c
    Signed-off-by: Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>

---
+++ atividade3/main.c
@@ -0,0 +1,24 @@
+#include "atividade3.h"
+
+int main() {
+  int i, MAX;
+
+  printf("Quantos valores você deseja armazenar no vetor?\n> ");
+  scanf("%d", &MAX);
+  int vet[MAX];
+
+  printf("\n");
+  for (i = 0; i < MAX; i++) {
+    printf("Digite o %dº valor: ", i + 1);
+    scanf("%d", &vet[i]);
+  }
+
+  ordenacao_shellsort(vet, MAX);
+
+  printf("Vetor ordenado: ");
+  for (i = 0; i < MAX; i++) {
+    printf("%d ", vet[i]);
+  }
+
+  return 0;
+}
