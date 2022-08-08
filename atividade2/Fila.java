34287b95766dc15cc93c6a7b868699acc2e68e70
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-08-07 23:47:30 -0300

    Update Fila.java
    Signed-off-by: Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>

--- atividade2/Fila.java
+++ atividade2/Fila.java
@@ -1,32 +1,36 @@
 public class Fila extends Lista {
-	
-	public Fila() {
-		super();
-	}
-	
-	public int get_topo() {
-		return this.get_tamanho();
-	}
-	
-	@Override
-	public void adiciona_no_inicio(Dado dado) {
-		System.out.printf("Método não implementado para Fila!\n");
-	}
-	
-	@Override
-	public void adiciona_na_posicao(int i, Dado dado) {
-		System.out.printf("Método não implementado para Fila!\n");		
-	}
-	
-	@Override
-	public Dado remove_do_fim() {
-		System.out.printf("Método não implementado para Fila!\n");
-		return null;
-	}
-	
-	@Override
-	public Dado remove_da_posicao(int i) {
-		System.out.printf("Método não implementado para Fila!\n");
-		return null;
-	}
+  // construtor
+  public Fila() {
+    // executa o construtor da superclasse
+    super();
+  }
+  
+  // métodos
+  public int get_topo() {
+    return this.get_tamanho();
+  }
+  
+  // métodos que serão sobrescritos
+  // pois não são utilizados na fila
+  @Override
+  public void adiciona_no_inicio(Dado dado) {
+    System.out.printf("Método não implementado para Fila!\n");
+  }
+  
+  @Override
+  public void adiciona_na_posicao(int i, Dado dado) {
+    System.out.printf("Método não implementado para Fila!\n");    
+  }
+  
+  @Override
+  public Dado remove_do_fim() {
+    System.out.printf("Método não implementado para Fila!\n");
+    return null;
+  }
+  
+  @Override
+  public Dado remove_da_posicao(int i) {
+    System.out.printf("Método não implementado para Fila!\n");
+    return null;
+  }
 }
