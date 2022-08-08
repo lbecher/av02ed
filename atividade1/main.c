a2362b4956f8955153ea8043864a10088dda64d2
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2022-08-08 02:27:53 -0300

    Update main.c
    Signed-off-by: Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>

--- atividade1/main.c
+++ atividade1/main.c
@@ -1,38 +1,40 @@
 #include "atividade1.h"
 
 int main() {
-  Grafo *gr;
-  int escolha = 0, max, i;
-  tipo_vertice v1, v2;
+  tipo_grafo *gr;
+  int escolha = 0, max, i, v1, v2;
   float peso;
-  printf("Inicializando o grafo!\nQual o número máximo de vértices do seu "
-         "grafo?\n");
+
+  printf("Inicializando o grafo!\nQual o número máximo de vértices do seu grafo?\n> ");
   scanf("%d", &max);
+
   gr = cria(max);
+
   do {
-    printf("Escolha a opcao que deseja realizar:\n1- inserir conexão no "
-           "grafo\n2- imprimir grafo \n3- finalizar programa\n");
-    scanf("%d", &escolha);
-    if (escolha < 0 || escolha > 3) {
-      printf("\n\nEscolha inválida. Digite 1, 2 ou 3\n");
-    }
+    do {
+      printf("\nEscolha a opcao que deseja realizar:\n 1. inserir conexão no grafo\n 2. imprimir grafo\n 3. finalizar programa\n> ");
+      scanf("%d", &escolha);
+      if (escolha < 0 || escolha > 3) {
+        printf("\nEscolha inválida!\n");
+      }
+    } while (escolha < 0 || escolha > 3);
+
     switch (escolha) {
-    case 1:
-      printf("Qual vértice deseja conectar?\n");
-      scanf("%d", &v1);
-      printf("Qual vértice é o adjacente de %d?\n", v1);
-      scanf("%d", &v2);
-      printf("Qual o peso?\n");
-      scanf("%f", &peso);
-      insere_conexao(gr, v1, v2, peso);
-      printf("Conexão inserida!\n");
-      break;
-    case 2:
-      imprime(gr);
-      break;
-    case 3:
-      printf("Até logo");
-      break;
+      case 1:
+        printf("Qual vértice deseja conectar?\n> ");
+        scanf("%d", &v1);
+        printf("Qual vértice é o adjacente de %d?\n> ", v1);
+        scanf("%d", &v2);
+        printf("Qual o peso?\n> ");
+        scanf("%f", &peso);
+        insere_aresta(gr, v1, v2, peso);
+        break;
+      case 2:
+        imprime(gr);
+        break;
+      case 3:
+        printf("Até logo!\n");
+        break;
     }
   } while (escolha != 3);
   return 0;
